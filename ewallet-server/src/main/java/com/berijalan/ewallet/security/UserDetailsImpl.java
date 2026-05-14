ff1a3b16be32ffd8323a7b732c490ed6bd058ae6
Abdi Setiawan <abdisetiawan.dev@gmail.com>
2026-05-15 06:46:14 +0700

    feat(security): refactor user details service and update JWT filter to use new implementation

--- ewallet-server/src/main/java/com/berijalan/ewallet/security/UserDetailsImpl.java
+++ ewallet-server/src/main/java/com/berijalan/ewallet/security/UserDetailsImpl.java
@@ -51,7 +51,7 @@ public class UserDetailsImpl implements UserDetails {
 
     @Override
     public String getUsername() {
-        return String.valueOf(id);
+        return email;
     }
 
     @Override
