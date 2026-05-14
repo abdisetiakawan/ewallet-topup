e4631ddd548b8797a165a02fbf21f59668ffc067
Abdi Setiawan <abdisetiawan.dev@gmail.com>
2026-05-14 11:32:05 +0700

    feat(db): add initial seed data for merchants and their taxes

--- ewallet-server/src/main/resources/db/migration/U1__initial_schema_indexes.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS trx_transactions;
-DROP TABLE IF EXISTS mst_wallets;
-DROP TABLE IF EXISTS mst_merchants;
-DROP TABLE IF EXISTS mst_users;
-
-DROP TYPE IF EXISTS transaction_status;
-DROP TYPE IF EXISTS transaction_type;
